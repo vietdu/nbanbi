9bc333740dcc9c297457db2fcb7b1fb4a27bdb4b
viet du <du.nguyenviet@hcmut.edu.vn>
2021-05-21 13:18:05 +0700

    vậy là hoàn thành, đã sửa phần khách sạn olala

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW Y HIDDEN =====*/
+
 const navMenu = document.getElementById('nav-menu'),
 
     toggleMenu = document.getElementById('nav-toggle'),
@@ -77,7 +77,7 @@ const navMenu = document.getElementById('nav-menu'),
    })
  
 
-// SHOW
+
 toggleMenu.addEventListener('click', ()=>{
    navMenu.classList.toggle('show')
 })
@@ -128,7 +128,6 @@ closebay.addEventListener('click', ()=>{
 
 
 
-// HIDDEN
 
 var rellax = new Rellax('.parallax');
 const srr = ScrollReveal({
@@ -139,11 +138,10 @@ const srr = ScrollReveal({
 })
 
 
-/*===== MOUSEMOVE HOME IMG =====*/
 document.addEventListener('mousemove', move);
 function move(e){
     this.querySelectorAll('.vietdu').forEach(layer =>{
-      //  const speed = layer.getAttribute('data-speed')
+ 
         const speed =layer.getAttribute('data-speed')
 
         const x = (-window.innerWidth  -e.pageX*speed)/120
@@ -154,7 +152,7 @@ function move(e){
     
 }
 
-/*SCROLL ABOUT*/
+
 srr.reveal('.about__img', {delay: 500})
 srr.reveal('.about__subtitle', {delay: 350})
 srr.reveal('.about__profession', {delay: 400})
@@ -184,13 +182,13 @@ function myFunction() {
    
  
     
-    if (((window.pageYOffset+130) > banner_zom2)) {
+    if (((window.pageYOffset+190) > banner_zom2)) {
       banner2.classList.add("banner_zom");
     } 
     else{
       banner2.classList.remove("banner_zom");
     }
-    if (((window.pageYOffset+130) > banner_zom3)) {
+    if (((window.pageYOffset+160) > banner_zom3)) {
       banner3.classList.add("banner_zom");
     } 
     else{
@@ -209,7 +207,7 @@ function myFunction() {
 
 
 
-/*contact*/
+
 
 srr.reveal('.contact__subtitle', {})
 srr.reveal('.contact__text', {interval: 200})
@@ -217,30 +215,27 @@ srr.reveal('.contact__input', {delay: 400})
 srr.reveal('.contact__button', {delay: 600})
 
 
-/*====== ANIMATE GSAP ======*/
-/*Navbar*/
+
+
 gsap.from('.nav__logo', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
 gsap.from('.nav__toggle', {opacity:0, duration: 3, delay: .7, y: 30, ease:'expo.out'});
 gsap.from('.nav__item', {opacity: 0, duration: 3, delay: .7, y: 35, ease:'expo.out', stagger: .2})
 
-/*Text*/
+
 
 gsap.from('.home__subtitle', {opacity:0, duration: 3, delay: 1.1 , y: 35, ease:'expo.out'});
 
-/*Scroll*/
+
 gsap.from('.home__scroll', {opacity:0, duration: 3, delay: 1.5, y: 25, ease:'expo.out'});
 
 
-/*====== SCROLL REVEAL SECTION ======*/
 const sr = ScrollReveal({
     duration: 2500,
     reset: true
 });
 
-/*Data*/
 sr.reveal('.section__data',{origin: 'left',distance: '70px'}); 
 
-/*Imgs*/
 sr.reveal('.section__img',{origin: 'left',distance: '90px',delay: 200}); 
 
 function myFunction1() {
@@ -281,5 +276,7 @@ const srr2 = ScrollReveal({
 })
 
 srr2.reveal('.form_bn3', {delay: 450})
+
+
 
 
